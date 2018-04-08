f9c401b65add598815c1bfa552feca20a63a1ec9
daniel kim <daniel.c.kim@live.com>
2018-04-08 15:34:57 -0700

    dsfsdf

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(props) {
       super(props);
       this.state = {
-        data: []
+        data: [],
+        newText: ""
       }
       this.loadData();
   }
@@ -26,10 +27,32 @@ class App extends Component {
         {this.state.data.map(doc => {
           return (
             <div>
-              <span>{doc.id}</span>
+              <span>{doc.id} - {doc.text}</span>
             </div>
           )
         })}
+
+        <input 
+          type="text" 
+          value={this.state.newText} 
+          onChange={ event => {
+            this.setState({newText:event.target.value});
+          }}
+        />
+        <button onClick={async ()=>{
+          let item = {text: this.state.newText};
+          let docref = await DataCollection.insert(item)
+          let newData = [...this.state.data];
+
+          item.id = docref.id;
+
+          newData.push(item);
+          this.setState({
+            data: newData,
+            newText: ""
+          });
+
+        }}>submit</button>
 
       </div>
     );
--- src/util/DataCollection.js
+++ src/util/DataCollection.js
@@ -2,24 +2,29 @@ import db from './db';
 
 export default class DataCollection {
     static list() {
-        return new Promise(resolve => {
+
+        // return db.collection("data").get();
+
+
+        return new Promise( async resolve => {
 
             let data = [];
-            db.collection("data").get().then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    let item = {
-                        id : doc.id,
-                        ...doc.data()
-                    }
-                    data.push(item);
-                });
-                resolve(data);
+            let querySnapshot = await db.collection("data").get();
+
+            querySnapshot.forEach((doc) => {
+                let item = {
+                    id : doc.id,
+                    ...doc.data()
+                }
+                data.push(item);
             });
+            resolve(data);
+            
         });
     }
 
     static insert(newObject) {
-
+        return db.collection("data").add(newObject);
     }
 
     static update(id, newObject) {
